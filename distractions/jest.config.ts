17c7fe66de732603fea80a769df072eed0928de2
Jeremy Clough <jeremy.clough@gmail.com>
2025-07-04 13:51:14 -0700

    partial fix

--- distractions/jest.config.ts
+++ distractions/jest.config.ts
@@ -2,13 +2,25 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@test/(.*)$': '<rootDir>/test/$1'
+    '^@test/(.*)$': '<rootDir>/test/$1',
+    '^@data/(.*)$': '<rootDir>/data/$1'
+    // '^civics2json/Questions$': '<rootDir>/../civics2json/dist/Questions.js',
+    // '^civics2json$': '<rootDir>/../civics2json/dist/types.js'
   },
   transform: {
-    // '^.+\.tsx?$': 'ts-jest', // default for ts-jest preset
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          moduleResolution: 'node'
+        }
+      }
+    ]
   },
   // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-};
+  clearMocks: true
+}
